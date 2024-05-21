29e35fd44961f0dcff7fbea783f841fe5b49d959
Alex Monroy <alexwebpuro@gmail.com>
2024-05-21 09:08:32 -0500

    Add: Remove reports, settings status and addons from admin location user role.

--- comercializadora-lmc.php
+++ comercializadora-lmc.php
@@ -15,41 +15,45 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
- function custom_roles_plugin_add_roles() {
-    // add_role(
-    //     'administrador_sucursal',
-    //     'Administrador de Sucursal',
-    //     array(
-    //         'read'                  => true,
-    //         'edit_posts'            => true, // Por ejemplo, no permitir la edición de publicaciones
-    //         'manage_woocommerce'    => true, // Capacidad para administrar WooCommerce
-    //     )
-    // );
-
-    remove_role('vendedor');
-    remove_role('administrador_sucursal');
+if(! defined('LMC_DOMAIN')) {
+    define('LMC_DOMAIN', 'comercializadora-lmc');
 }
-add_action('init', 'custom_roles_plugin_add_roles');
 
-// Agregar campo "Sucursal' en la página de perfil de usuario
+     
+function custom_roles_plugin_add_roles() {
+   // add_role(
+   //     'administrador_sucursal',
+   //     'Administrador de Sucursal',
+   //     array(
+   //         'read'                  => true,
+   //         'edit_posts'            => true, // Por ejemplo, no permitir la edición de publicaciones
+   //         'manage_woocommerce'    => true, // Capacidad para administrar WooCommerce
+   //     )
+   // );
+   remove_role('vendedor');
+   remove_role('administrador_sucursal');
+}
+
+/**
+ * Add field Location on User profile.
+ */
 function custom_roles_plugin_add_custom_user_profile_fields($user) {
     $selected_term = get_user_meta($user->ID, 'sucursal', true); // Obtener el término seleccionado para el usuario
 
-    // Obtener las taxonomías de WooCommerce
     $args = array(
         'taxonomy' => 'location',
         'hide_empty' => false,
     );
-    $terms = get_terms($args);
+    $terms = get_terms($args); // Get Location taxonomy
     
     ?>
-    <h3><?php _e('Sucursal', 'custom-roles-plugin'); ?></h3>
-    <table class="form-table">
+    <h3><?php _e('Sucursal', LMC_DOMAIN); ?></h3>
+    <table class="form-table" id="Location-table">
         <tr>
-            <th><label for="sucursal"><?php _e('Seleccione la sucursal', 'custom-roles-plugin'); ?></label></th>
+            <th><label for="sucursal"><?php _e('Seleccione la sucursal', LMC_DOMAIN); ?></label></th>
             <td>
                 <select name="sucursal" id="sucursal" class="select2">
-                    <option value=""><?php _e('Seleccione una sucursal', 'custom-roles-plugin'); ?></option>
+                    <option value=""><?php _e('Seleccione una sucursal', LMC_DOMAIN); ?></option>
                     <?php foreach ($terms as $term) : ?>
                         <option value="<?php echo esc_attr($term->term_id); ?>" <?php selected($selected_term, $term->term_id); ?>><?php echo esc_html($term->name); ?></option>
                     <?php endforeach; ?>
@@ -62,7 +66,9 @@ function custom_roles_plugin_add_custom_user_profile_fields($user) {
 add_action('show_user_profile', 'custom_roles_plugin_add_custom_user_profile_fields');
 add_action('edit_user_profile', 'custom_roles_plugin_add_custom_user_profile_fields');
 
-// Guardar el valor del campo "Sucursal" al actualizar el perfil de usuario
+/**
+ * Save value field "Location" to update user profile.
+ */
 function custom_roles_plugin_save_custom_user_profile_fields($user_id) {
     if (!current_user_can('edit_user', $user_id)) {
         return false;
@@ -75,31 +81,21 @@ function custom_roles_plugin_save_custom_user_profile_fields($user_id) {
 add_action('personal_options_update', 'custom_roles_plugin_save_custom_user_profile_fields');
 add_action('edit_user_profile_update', 'custom_roles_plugin_save_custom_user_profile_fields');
 
-function lmc_test() {
-    $user_id = get_current_user_id(); // Obtener el ID del usuario actualmente autenticado
 
-    $user_meta = get_user_meta($user_id);
-
-    // dump($user_meta, $user_id, get_userdata( get_current_user_id() )->allcaps);
-}
-
-add_action('wp_footer', 'lmc_test', 10, 0 );
-
-
-function dcms_insertar_js(){
+/**
+ * Load custom scripts.
+ */
+function lmc_sctipts(){
     
     $user_id = get_current_user_id();
 
-    $location_id = get_user_meta($user_id, 'sucursal', true);
-    // dump($location_id);
-    
+    $location_id = get_user_meta($user_id, 'sucursal', true);    
 	wp_register_script('location', plugin_dir_url( __FILE__ ) . '/assets/js/frontend.js', array('jquery'), '', true );
 	wp_enqueue_script('location');
     
 	wp_localize_script('location','locationID',['id'=>$location_id]);
 }
-//Insertar Javascript js y enviar ruta admin-ajax.php
-add_action('wp_enqueue_scripts', 'dcms_insertar_js');
+add_action('wp_enqueue_scripts', 'lmc_sctipts');
 
 /**
 * Add a custom field to the checkout page
@@ -137,7 +133,9 @@ function custom_checkout_field($checkout) {
 
 add_action('woocommerce_after_checkout_billing_form', 'custom_checkout_field');
 
-// Save the dropdown custom field selected value as order custom meta data:
+/**
+ * Save the dropdown custom field selected value as order custom meta data:
+ */
 add_action( 'woocommerce_checkout_create_order', 'my_custom_checkout_field_update_order_meta', 10, 2 );
 function my_custom_checkout_field_update_order_meta( $order, $data ) {
     if ( isset($_POST['location']) && ! empty($_POST['location']) ) {
@@ -145,19 +143,25 @@ function my_custom_checkout_field_update_order_meta( $order, $data ) {
     } 
 }
 
-// Display the custom field value on admin order pages after billing adress:
+/**
+ * Display the custom field value on admin order pages after billing adress:
+ */
 add_action( 'woocommerce_admin_order_data_after_billing_address', 'my_custom_checkout_field_display_admin_order_meta', 10, 1 );
 function my_custom_checkout_field_display_admin_order_meta( $order ) {
     echo '<p><strong>'.__('Location').':</strong> ' . $order->get_meta('location') . '</p>'; 
 }
 
-// Display the custom field value on email notifications:
+/**
+ * Display the custom field value on email notifications:
+ */
 add_action( 'woocommerce_email_after_order_table', 'custom_woocommerce_email_order_meta_fields', 10, 4 );
 function custom_woocommerce_email_order_meta_fields( $order, $sent_to_admin, $plain_text, $email ) {
     echo '<p><strong>'.__('Location').':</strong> ' . $order->get_meta('location') . '</p>';
 }
 
-// Agregar el filtro en la pantalla de órdenes de pedido
+/**
+ * Agregar el filtro en la pantalla de órdenes de pedido
+ */
 function agregar_filtro_location_select2() {
     global $typenow;
     if ( 'shop_order' === $typenow ) {
@@ -167,7 +171,7 @@ function agregar_filtro_location_select2() {
         ) );
 
         echo '<select name="location_filter" id="dropdown_location_filter">';
-        echo '<option value="">' . __( 'Filter by location', 'woocommerce' ) . '</option>';
+        echo '<option value="">' . __( 'Filtrar por sucursal', LMC_DOMAIN ) . '</option>';
         
         foreach ( $terms as $term ) {
             $selected = isset($_GET['location_filter']) && $_GET['location_filter'] == $term->term_id ? 'selected' : '';
@@ -186,7 +190,9 @@ function agregar_filtro_location_select2() {
 }
 add_action( 'restrict_manage_posts', 'agregar_filtro_location_select2', 20 );
 
-// Modificar la consulta de pedidos para incluir el filtro
+/**
+ * Modificar la consulta de pedidos para incluir el filtro
+ */
 function filtrar_ordenes_por_location( $query ) {
     global $pagenow, $typenow;
 
@@ -203,7 +209,25 @@ function filtrar_ordenes_por_location( $query ) {
 }
 add_action( 'pre_get_posts', 'filtrar_ordenes_por_location' );
 
-// Cargar select2 en el admin
+function lmc_hide_nenus_to_seller() {
+    if ( current_user_can( 'admin_location' ) ) {
+        remove_menu_page( 'woocommerce' ); // WooCommerce main menu
+
+        // Submenús específicos
+        remove_submenu_page( 'woocommerce', 'wc-admin&path=/analytics/overview' ); // Analytics
+        remove_submenu_page( 'woocommerce', 'wc-reports' ); // Reports
+        remove_submenu_page( 'woocommerce', 'wc-settings' ); // Settings
+        remove_submenu_page( 'woocommerce', 'wc-status' ); // Status
+        remove_submenu_page( 'woocommerce', 'wc-addons' ); // Addons
+
+        // Si quieres eliminar otros submenús de WooCommerce, puedes agregar más líneas aquí.
+    }
+}
+add_action( 'admin_menu', 'lmc_hide_nenus_to_seller', 99 );
+
+/**
+ * Cargar select2 en el admin
+ */
 function cargar_select2_admin() {
     global $typenow;
     if ( 'shop_order' === $typenow ) {
@@ -211,7 +235,7 @@ function cargar_select2_admin() {
         wp_enqueue_style( 'select2-css', plugins_url( '/select2/css/select2.min.css', __FILE__ ), array(), '4.0.13' );
     }
 }
-add_action( 'admin_enqueue_scripts', 'cargar_select2_admin' );
+// add_action( 'admin_enqueue_scripts', 'cargar_select2_admin' );
 
 
 add_action('wp_footer', function() {
@@ -220,3 +244,12 @@ add_action('wp_footer', function() {
     dump($order);
 });
 
+function lmc_test() {
+    $user_id = get_current_user_id(); // Obtener el ID del usuario actualmente autenticado
+
+    $user_meta = get_user_meta($user_id);
+
+    // dump($user_meta, $user_id, get_userdata( get_current_user_id() )->allcaps);
+}
+
+// add_action('wp_footer', 'lmc_test', 10, 0 );
